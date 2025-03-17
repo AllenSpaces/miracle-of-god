98f8ccc8211aa1cc3daff4ba23ef094f353cce66
Alan-Kuohao <152664179+wukuohao2003@users.noreply.github.com>
2025-03-17 14:11:42 +0800

    Fix

--- lua/miracle-of-god/utils/entrance.lua
+++ lua/miracle-of-god/utils/entrance.lua
@@ -6,13 +6,22 @@ function M.entrancePage(config)
 	local width = vim.opt.columns
 	local height = vim.opt.lines
 
-	local start_col = math.floor((width - #assic) / 2)
-	local start_row = math.floor(height / 2)
+	local max_line_length = 0
+	for _, line in ipairs(assic) do
+		if #line > max_line_length then
+			max_line_length = #line
+		end
+	end
+	local start_col = math.floor((width - max_line_length) / 2)
+
+	local start_row = math.floor((height - #assic) / 2)
 
 	vim.api.nvim_command("clear")
 
-	vim.api.nvim_buf_set_lines(0, start_row, start_row, false, { assic })
-	vim.api.nvim_buf_add_highlight(0, -1, "Title", start_row, start_col, start_col + #assic)
+	for i, line in ipairs(assic) do
+		vim.api.nvim_buf_set_lines(0, start_row + i - 1, start_row + i - 1, false, { line })
+		vim.api.nvim_buf_add_highlight(0, -1, "Title", start_row + i - 1, start_col, start_col + #line)
+	end
 end
 
 return M
