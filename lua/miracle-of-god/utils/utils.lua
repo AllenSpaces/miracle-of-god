15bb0a98bdbbf2e96292286b90493d5d0a1f54f4
Alan-Kuohao <152664179+wukuohao2003@users.noreply.github.com>
2025-03-18 14:04:20 +0800

    Add markdown syntax support

--- lua/miracle-of-god/utils/utils.lua
+++ lua/miracle-of-god/utils/utils.lua
@@ -163,6 +163,12 @@ function M.setHighLight()
 	HL("bashTest", { fg = colors.GREEN })
 	HL("bashBuiltin", { fg = colors.PINK })
 	HL("bashSpecial", { fg = colors.ORANGE })
+	HL("markdownH1", { fg = colors.RED, bold = true })
+	HL("markdownH2", { fg = colors.ORANGE, bold = true })
+	HL("markdownH3", { fg = colors.YELLOW, bold = true })
+	HL("markdownH4", { fg = colors.GREEN, bold = true })
+	HL("markdownH5", { fg = colors.PINK, bold = true })
+	HL("markdownH6", { fg = colors.PURPLE, bold = true })
 end
 
 return M
