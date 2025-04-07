5ea42e1da28ec9c75db954e3818ead4c6a8a20a6
吴阔豪 <wukuohao@wukuohaodeMini.lan>
2025-04-07 22:56:31 +0800

    Change some

--- lua/miracle-of-god/init.lua
+++ lua/miracle-of-god/init.lua
@@ -5,11 +5,9 @@ M.setup = function(USER_CONFIG)
 		vim.tbl_deep_extend("force", require("miracle-of-god.utils.config").DEFAULT_CONFIG, USER_CONFIG or {})
 
 	vim.o.background = config.dark and "dark" or "light"
+	vim.cmd.hi("clear")
+	require("miracle-of-god.utils.utils").setHighLight()
 	vim.g.colors_name = "miracle-of-god"
-	if vim.g.colors_name then
-		vim.cmd.hi("clear")
-		require("miracle-of-god.utils.utils").setHighLight()
-	end
 end
 
 return M
